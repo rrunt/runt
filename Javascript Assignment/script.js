a76bdba8bad905ef7602758c40fb7082c7aa1018
rrunt <112667235+rrunt@users.noreply.github.com>
2023-02-04 03:30:41 -0500

    Add files via upload
    JavaScript Intro Assessment

---
+++ Javascript Assignment/script.js
@@ -0,0 +1,14 @@
+var firstName;
+firstName = 'Luis';
+var lastName;
+lastName = 'Romero';
+var age;
+age = '21';
+
+
+alert("My name is " + firstName + " " + lastName);
+alert("I am " + age + " years old");
+
+
+
+console.log("I am having a lot of fun in this class learning about Javascript.")
